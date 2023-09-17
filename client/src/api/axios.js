479aefe2b50e96c7b82e84ef642470a9852351f8
Elvis Josue <elvisjosue98@hotmail.com>
2023-09-17 17:32:00 -0600

    Cambiando turas de peticiones al backend 2

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://consultor-es.vercel.app/api",
-  // baseURL: "http://localhost:4000/api",
+  baseURL: "http://localhost:4000/api",
   withCredentials: true,
 });
 
