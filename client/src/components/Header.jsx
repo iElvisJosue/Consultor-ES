10dae71252992f396da8a2b95ab7a845a3859782
Elvis Josue <elvisjosue98@hotmail.com>
2023-09-18 19:26:25 -0600

    Se agrego un menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,48 +1,67 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const classMenu = showMenu
+    ? "Main__Header--Navbar Show"
+    : "Main__Header--Navbar";
+
   return (
-    <header className="Main__Header">
-      <a href="/" className="Main__Header--Picture">
-        <img
-          src="./LogoConsultores.png"
-          alt="Logo de la empresa"
-          className="Main__Header--Picture--Img"
-        />
-      </a>
-      <ul className="Main__Header--Menu">
-        <li className="Main__Header--Menu--Item">
-          <a href="/WhyConsultant" className="Main__Header--Menu--Item--Link">
-            ¿Para qué un consultor?
-          </a>
-        </li>
-        <li className="Main__Header--Menu--Item">
-          <a href="#" className="Main__Header--Menu--Item--Link">
-            ¿Quieres ser un consultor?
-          </a>
-        </li>
-        <li className="Main__Header--Menu--Item">
-          <a href="#" className="Main__Header--Menu--Item--Link">
-            ¿Tienes un proyecto?
-          </a>
-        </li>
-        <li className="Main__Header--Menu--Item">
-          <a href="/About" className="Main__Header--Menu--Item--Link">
-            ¿Quienes somos?
-          </a>
-        </li>
-        <li className="Main__Header--Menu--Item">
-          <a href="#" className="Main__Header--Menu--Item--Link">
-            Pregúntanos
-          </a>
-        </li>
-        <li className="Main__Header--Menu--Item">
-          <a href="/Login" className="Main__Header--Menu--Item--Link">
-            Iniciar sesión
-          </a>
-        </li>
-      </ul>
-      <button className="Main__Header--Button">
-        <ion-icon name="filter-outline"></ion-icon>
-      </button>
-    </header>
+    <>
+      <header className="Main__Header">
+        <a href="/" className="Main__Header--Picture">
+          <img
+            src="./LogoConsultores.png"
+            alt="Logo de la empresa"
+            className="Main__Header--Picture--Img"
+          />
+        </a>
+        <ul className="Main__Header--Menu">
+          <li className="Main__Header--Menu--Item">
+            <a href="/WhyConsultant" className="Main__Header--Menu--Item--Link">
+              ¿Para qué un consultor?
+            </a>
+          </li>
+          <li className="Main__Header--Menu--Item">
+            <a href="#" className="Main__Header--Menu--Item--Link">
+              ¿Quieres ser un consultor?
+            </a>
+          </li>
+          <li className="Main__Header--Menu--Item">
+            <a href="#" className="Main__Header--Menu--Item--Link">
+              ¿Tienes un proyecto?
+            </a>
+          </li>
+          <li className="Main__Header--Menu--Item">
+            <a href="/About" className="Main__Header--Menu--Item--Link">
+              ¿Quienes somos?
+            </a>
+          </li>
+          <li className="Main__Header--Menu--Item">
+            <a href="#" className="Main__Header--Menu--Item--Link">
+              Pregúntanos
+            </a>
+          </li>
+          <li className="Main__Header--Menu--Item">
+            <a href="/Login" className="Main__Header--Menu--Item--Link">
+              Iniciar sesión
+            </a>
+          </li>
+        </ul>
+        <button
+          className="Main__Header--Button"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          <ion-icon name="filter-outline"></ion-icon>
+        </button>
+      </header>
+      <div className={classMenu}>
+        <button onClick={() => setShowMenu(!showMenu)}>Cerrar</button>
+        <a href="/About">¿Quienes somos?</a>
+        <a href="/WhyConsultant">¿Para qué un consultor?</a>
+        <a href="/Login">Iniciar sesión</a>
+      </div>
+    </>
   );
 }
